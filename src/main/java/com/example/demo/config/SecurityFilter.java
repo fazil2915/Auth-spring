eb9bb7feb20f8e62765607dea88011d3c09d4977
fazil2915 <fazilrah9846@gmail.com>
2024-11-19 18:07:10 +0530

    Custom-service

--- src/main/java/com/example/demo/config/SecurityFilter.java
+++ src/main/java/com/example/demo/config/SecurityFilter.java
@@ -1,28 +1,46 @@
 package com.example.demo.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
     @Bean
-    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http)throws  Exception{
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    return http
-            .csrf(customizer->customizer.disable())
-            .authorizeHttpRequests(request->request.anyRequest().authenticated())
-           //http.formLogin(Customizer.withDefaults());
-            .httpBasic(Customizer.withDefaults())
-            .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        return http
+                .csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                //.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-    .build();
-}
+                .build();
+    }
+//custom database service to set manual
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
 
-}
+
+    }
--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.Student;
+import com.example.demo.models.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.PostMapping;
